7db75d49810e428ac5b3489ad5e3780ea7200b3b
nis923 <74984118+nis923@users.noreply.github.com>
2020-11-28 18:11:26 +0530

    Add files via upload

---
+++ task2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+int main()
+  {
+        int n,k,i,m,j = 0;
+char s,y;
+
+        printf("enter the length of word and number of letters ");
+        scanf("%d %d",&n,&k);
+        printf("enter the word" );
+        scanf(" %s", &y);
+        printf("enter your characters");
+        scanf(" %s ",&s);
+
+for(int i=1;i < n;i++)
+{
+for(int m=1;m < k;m++)
+{
+if(s.substr[k]=y.substr[n])
+
+        j++;
+}
+}
+printf ("%d",j);
+return 0;
+}
